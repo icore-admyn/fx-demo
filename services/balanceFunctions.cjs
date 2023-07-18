175bf963ebe449ea370cc2e5bd0cb1032e79f160
ryansallen98 <115708378+ryansallen98@users.noreply.github.com>
2023-07-18 22:02:23 +0100

    Updated functions

--- services/balanceFunctions.cjs
+++ services/balanceFunctions.cjs
@@ -40,6 +40,7 @@ const newParams = async (relayUrl, walletAddress, amount, originalAmount) => {
   // Generate a random invoice ID & custom order ID
   const invoiceId = Math.random().toString(36).substring(7);
   const customOrderId = Math.random().toString(36).substring(7);
+  console.log(originalAmount)
 
   // Set up url params
   const params = {
