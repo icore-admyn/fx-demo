9de312ad4f2c47993b27cd80f09fcb749d4b27cb
ryansallen98 <115708378+ryansallen98@users.noreply.github.com>
2023-07-18 22:21:37 +0100

    Updated functions

--- services/ipnFunctions.cjs
+++ services/ipnFunctions.cjs
@@ -1,3 +1,5 @@
+const axios = require('axios');
+
 const sendUpdate = async (paymentId, user) => {
     const response = await axios.post('http://3.87.213.174:5372//transaction/update', { paymentId: paymentId, status: 'paid' }, {
         headers: {
