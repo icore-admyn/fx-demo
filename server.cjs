93100afd22bcf12e5a493a24e875e10520025c6d
ryansallen98 <115708378+ryansallen98@users.noreply.github.com>
2023-07-18 12:51:20 +0100

    Updated server.cjs

--- server.cjs
+++ server.cjs
@@ -9,7 +9,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, './dist')));
-app.use(express.static(path.join(__dirname, 'public')));
 
 // View engine setup
 app.set('views', path.join(__dirname, 'views'));
