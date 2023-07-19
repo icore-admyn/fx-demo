c7a18a0e13391a4e330ff5c2d4d144955e3968fe
ryansallen98 <115708378+ryansallen98@users.noreply.github.com>
2023-07-19 01:00:24 +0100

    Update IPN

--- services/ipnFunctions.cjs
+++ services/ipnFunctions.cjs
@@ -1,7 +1,8 @@
 const axios = require('axios');
 
 const sendUpdate = async (paymentId, user) => {
-    const response = await axios.post('http://3.87.213.174:5372/transaction/update', { paymentId: paymentId, status: 'paid' }, {
+    console.log(paymentId, user)
+    const response = await axios.post('http://3.87.213.174:5372/transaction/update', { "paymentId": paymentId, "status": "paid" }, {
         headers: {
             Authorization: `Bearer ${user.key}`,
         },
