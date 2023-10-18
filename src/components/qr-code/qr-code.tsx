ee794e3f61957b34cc69870847568c6cc319f499
ryansallen98 <115708378+ryansallen98@users.noreply.github.com>
2023-10-18 18:53:37 +0100

    Update QR Code Component

--- src/components/qr-code/qr-code.tsx
+++ src/components/qr-code/qr-code.tsx
@@ -1,26 +1,71 @@
+import React, { useEffect, useState } from 'react';
 import classnames from 'classnames';
 import styles from './qr-codes.module.scss';
 import globalStyles from '../../css/global.module.scss';
 import QRCode from 'qrcode.react';
+import axios from 'axios';
 
 export interface QrCodeProps {
     setQrCodePopUp: (value: boolean) => void;
     url: string;
-    setForceUpdate: any
+    setForceUpdate: any;
 }
 
-const QrCode = ({ setQrCodePopUp, url, setForceUpdate }: QrCodeProps) => {
+const QrCode: React.FC<QrCodeProps> = ({ setQrCodePopUp, url, setForceUpdate }) => {
 
-    function HandlePopUp() {
+    const paymentId: any = url.split('/').pop();
+    const webSocketUrl = url.replace("http", "ws");
+
+    function HandleClose() {
         setForceUpdate((prev: any) => !prev);
         setQrCodePopUp(false);
     }
 
+    async function fetchStatus() {
+        const headers = { Accept: 'application/payment-request' };
+        const response = await axios.get(url, { headers });
+        const data = response.data;
+        if (data.status === 'paid') {
+            alert('Payment received');
+        } else{
+            alert('There was an error with the payment');
+        }
+        console.log(data)
+    }
+
+    useEffect(() => {
+        const socket = new WebSocket(webSocketUrl);
+        socket.addEventListener("open", () => {
+            console.log("WebSocket connection established.");
+            socket.send(paymentId.toString());
+        });
+
+        socket.addEventListener("message", (event) => {
+            const jsonString = event.data
+            const jsonObject = JSON.parse(jsonString);
+            console.log("Received message from server:", jsonObject.status);
+        });
+
+        socket.addEventListener("close", (event) => {
+            if (event.wasClean) {
+                console.log("WebSocket connection closed via client");
+            } else {
+                console.log("WebSocket connection closed via server");
+                fetchStatus();
+            }
+        });
+
+
+        return () => {
+            socket.close();
+        };
+    }, []);
+
     return (
         <section className={globalStyles.popUpWrapper}>
-            <div onClick={HandlePopUp} className={globalStyles.popUpBackground} />
+            <div onClick={HandleClose} className={globalStyles.popUpBackground} />
             <div className={classnames(styles.popUpCard)}>
-                <button onClick={HandlePopUp} className={globalStyles.popUpClose} />
+                <button onClick={HandleClose} className={globalStyles.popUpClose} />
                 <div className={styles.checkoutWrapper}>
                     <QRCode value={url} />
                 </div>
