555d39e9936ffb3ba7420c62a91e4cd59716ede6
ryansallen98 <115708378+ryansallen98@users.noreply.github.com>
2023-10-18 14:59:42 +0100

    update Admyn

--- services/ipnFunctions.cjs
+++ services/ipnFunctions.cjs
@@ -3,7 +3,7 @@ const { findUser } = require('../services/userFunctions.cjs')
 
 const sendUpdate = async (paymentId, key) => {
     try {
-        const url = 'http://3.87.213.174:5372/transaction/update';
+        const url = 'http://3.82.93.211/:5372/transaction/update';
         const headers = { Authorization: `Bearer ${key}` };
         const body = { paymentId: paymentId, status: "paid" };
         const response = await axios.post(url, body, { headers });
